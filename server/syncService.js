f6c88ddf6526ea25fbd5669caa78c476137c004b
Kuybio <veriko10c@gmail.com>
2023-12-18 02:30:16 +0800

    Push 18
    Another update for syncService.js

--- server/syncService.js
+++ server/syncService.js
@@ -32,9 +32,15 @@ const fetchUnsyncedChanges = () => {
 const syncNewStudentsWithBlockchain = async () => {
     const newStudents = await fetchNewStudents();
     for (const student of newStudents) {
+        // Validate student data
+        if (!student.studentId || !student.name || !student.programme || !student.joinYear || student.cgpa == null || student.graduateYear == null) {
+            console.error(`Invalid data for student ${student.studentId}`);
+            continue; // Skip this student and continue with the next one
+        }
+
         try {
             await blockchain.addStudent(student.studentId, student.name, student.programme, student.joinYear, student.cgpa, student.graduateYear);
-            // Mark synced in database...
+            // Mark as synced...
         } catch (error) {
             console.error(`Failed to sync new student ${student.studentId}:`, error);
         }
