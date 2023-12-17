aa9549f4f5422f682defd1d933f9301fcc47db0c
Kuybio <veriko10c@gmail.com>
2023-12-18 00:57:12 +0800

    Push 2
    Update in Smart Contract

--- contracts/UniversityData.sol
+++ contracts/UniversityData.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract UniversityDataV2 {
+contract UniversityData {
     struct Student {
         uint256 studentId;
         string name;
@@ -9,24 +9,31 @@ contract UniversityDataV2 {
         uint256 joinYear;
         uint256 cgpa;
         uint256 graduateYear;
+        bytes32 dataHash; // SHA-256 hash of the student data
     }
 
     mapping(uint256 => Student) private students;
-    event StudentAdded(uint256 indexed studentId);
-    event StudentUpdated(uint256 indexed studentId);
+    event StudentAdded(uint256 indexed studentId, bytes32 dataHash);
+    event StudentUpdated(uint256 indexed studentId, bytes32 dataHash);
 
     // Add a new student
     function addStudent(uint256 _studentId, string memory _name, string memory _programme, uint256 _joinYear, uint256 _cgpa, uint256 _graduateYear) public {
         require(students[_studentId].studentId == 0, "Student already exists");
-        students[_studentId] = Student(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear);
-        emit StudentAdded(_studentId);
+        
+        bytes32 dataHash = sha256(abi.encodePacked(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear));
+        students[_studentId] = Student(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear, dataHash);
+        
+        emit StudentAdded(_studentId, dataHash);
     }
 
     // Update an existing student
     function updateStudent(uint256 _studentId, string memory _name, string memory _programme, uint256 _joinYear, uint256 _cgpa, uint256 _graduateYear) public {
         require(students[_studentId].studentId != 0, "Student does not exist");
-        students[_studentId] = Student(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear);
-        emit StudentUpdated(_studentId);
+        
+        bytes32 dataHash = sha256(abi.encodePacked(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear));
+        students[_studentId] = Student(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear, dataHash);
+        
+        emit StudentUpdated(_studentId, dataHash);
     }
 
     // Get student data
