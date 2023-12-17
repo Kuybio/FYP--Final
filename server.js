75a81e94fbd2235ae27ca7658b708575bc4d985c
Kuybio <veriko10c@gmail.com>
2023-12-18 01:40:31 +0800

    Push 11
    another update on server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Web3 = require('web3');
 const schedule = require('node-schedule');
 const blockchain = require('./server/blockchain');
 const database = require('./server/database');
-const { syncWithBlockchain } = require('./server/syncService');
+const syncService = require('./server/syncService'); // Import syncService as a whole
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,38 +21,38 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
 
 // Schedule a job to sync with the blockchain periodically
 const syncJob = schedule.scheduleJob('*/1 * * * *', function() {
-  console.log('Running Sync Job');
-  syncWithBlockchain(database, blockchain);
+    console.log('Running Sync Job');
+    syncService.syncNewStudentsWithBlockchain();
+    syncService.syncUpdatesWithBlockchain();
 });
 
 // Endpoint to add a student (can be used if manual addition is needed)
 app.post('/api/addStudent', async (req, res) => {
-  // Extract student data from the request body
-  const { studentId, name, programme, joinYear, cgpa, graduateYear } = req.body;
-  
-  try {
-    const result = await blockchain.addStudent(studentId, name, programme, joinYear, cgpa, graduateYear);
-    res.json({ success: true, message: 'Student added successfully!', transactionHash: result.transactionHash });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Error adding student', error: error.message });
-  }
+    const { studentId, name, programme, joinYear, cgpa, graduateYear } = req.body;
+    
+    try {
+        const result = await blockchain.addStudent(studentId, name, programme, joinYear, cgpa, graduateYear);
+        res.json({ success: true, message: 'Student added successfully!', transactionHash: result.transactionHash });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Error adding student', error: error.message });
+    }
 });
 
 // Endpoint to get a student's data
 app.get('/api/getStudent/:studentId', async (req, res) => {
-  const { studentId } = req.params;
-  
-  try {
-    const studentData = await blockchain.getStudent(studentId);
-    res.json({ success: true, studentData });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Error retrieving student data', error: error.message });
-  }
+    const { studentId } = req.params;
+    
+    try {
+        const studentData = await blockchain.getStudent(studentId);
+        res.json({ success: true, studentData });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Error retrieving student data', error: error.message });
+    }
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
