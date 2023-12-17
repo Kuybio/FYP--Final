3cc6c420136c526f80370a86651ddddb913f4595
Kuybio <veriko10c@gmail.com>
2023-12-18 04:31:49 +0800

    Push 28
    Update for blockchain.js

--- server/blockchain.js
+++ server/blockchain.js
@@ -1,52 +1,35 @@
 const Web3 = require('web3');
-const { abi } = require('../build/contracts/UniversityData.json'); // Update path to ABI file
+const UniversityDataContract = require('./UniversityDataContract.json');
 
-const web3 = new Web3('http://localhost:7545'); // Update with your provider URL
-const contractAddress = '0x6c35F6F7F727A7bB676e5BB73Ff66C6240D5B50c'; // Update with your contract address
-const privateKey = '0x6449d67debeb3b9471474b20b74b5e04ebbcf4c97ff48c8dde24b1257f07ad6b'; // Update with your private key
+const web3 = new Web3('http://localhost:7545'); // Adjust with your Ethereum client URL
+const contractAddress = 'YOUR_CONTRACT_ADDRESS'; // Replace with your contract address
+const contract = new web3.eth.Contract(UniversityDataContract.abi, contractAddress);
 
-const universityDataContract = new web3.eth.Contract(abi, contractAddress);
-
-async function addStudent(studentId, name, programme, joinYear, cgpa, graduateYear) {
-    // No longer throwing an error for null cgpa or graduateYear as they are handled in syncService.js
-    const fromAddress = web3.eth.accounts.privateKeyToAccount(privateKey).address;
-    const data = universityDataContract.methods.addStudent(studentId, name, programme, joinYear, cgpa, graduateYear).encodeABI();
-    const tx = {
-        from: fromAddress,
-        to: contractAddress,
-        data: data,
-        gas: 2000000,
-    };
-
-    return sendTransaction(tx, privateKey);
-}
-
-async function updateStudent(studentId, name, programme, joinYear, cgpa, graduateYear) {
-    const fromAddress = web3.eth.accounts.privateKeyToAccount(privateKey).address;
-    const data = universityDataContract.methods.updateStudent(studentId, name, programme, joinYear, cgpa, graduateYear).encodeABI();
-    const tx = {
-        from: fromAddress,
-        to: contractAddress,
-        data: data,
-        gas: 2000000,
-    };
-    return sendTransaction(tx, privateKey);
-}
-
-async function estimateGas(fromAddress, data) {
-    return await web3.eth.estimateGas({
-        from: fromAddress,
-        to: contractAddress,
-        data: data,
-    });
-}
-
-async function sendTransaction(tx, privateKey) {
+const sendTransaction = async (tx, privateKey) => {
     const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
     return web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-}
-
-module.exports = {
-    addStudent,
-    updateStudent,
 };
+
+const addStudent = async (fromAddress, privateKey, studentId, name, programme, joinYear, cgpa, graduateYear) => {
+    const data = contract.methods.addStudent(studentId, name, programme, joinYear, cgpa, graduateYear).encodeABI();
+    const tx = {
+        from: fromAddress,
+        to: contractAddress,
+        data: data,
+        gas: 2000000,
+    };
+    return sendTransaction(tx, privateKey);
+};
+
+const updateStudent = async (fromAddress, privateKey, studentId, name, programme, joinYear, cgpa, graduateYear) => {
+    const data = contract.methods.updateStudent(studentId, name, programme, joinYear, cgpa, graduateYear).encodeABI();
+    const tx = {
+        from: fromAddress,
+        to: contractAddress,
+        data: data,
+        gas: 2000000,
+    };
+    return sendTransaction(tx, privateKey);
+};
+
+module.exports = { addStudent, updateStudent };
