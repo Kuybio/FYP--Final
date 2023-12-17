af46df27afbb8ac42e83591bf531405d882be66f
Kuybio <veriko10c@gmail.com>
2023-12-18 06:52:51 +0800

    Push 45
    changing the univeristydatatest.js

--- test/UniversityDataTest.js
+++ test/UniversityDataTest.js
@@ -7,38 +7,35 @@ contract("UniversityData", accounts => {
         universityData = await UniversityData.deployed();
     });
 
-    it("should add a new student", async () => {
-        await universityData.addOrUpdateStudent(1, "Alice", "Computer Science", 2021, 350, 0);
-        let student = await universityData.getStudent(1);
-        
-        assert.equal(student.name, "Alice", "The name of the student should be Alice");
-        assert.equal(student.programme, "Computer Science", "The programme should be Computer Science");
+    describe("addStudent", () => {
+        it("should add a new student", async () => {
+            const studentId = 1;
+            const name = "Alice";
+            const programme = "Computer Science";
+            const joinYear = 2020;
+            const cgpa = 350;
+            const graduateYear = 2024;
+
+            await universityData.addStudent(studentId, name, programme, joinYear, cgpa, graduateYear);
+
+            const student = await universityData.getStudent(studentId);
+            assert.equal(student.name, name, "Name should match");
+            // Add other assertions as needed
+        });
     });
 
-    it("should update an existing student", async () => {
-        await universityData.addOrUpdateStudent(1, "Alice", "Mathematics", 2021, 370, 2024);
-        let student = await universityData.getStudent(1);
+    describe("updateStudent", () => {
+        it("should update an existing student", async () => {
+            const studentId = 1;
+            const updatedCgpa = 380;
 
-        // Assuming graduateYear is returned as a simple number
-        assert.equal(student.graduateYear, 2024, "The graduation year should be updated to 2024");
-        assert.equal(student.programme, "Mathematics", "The programme should be updated to Mathematics");
+            await universityData.updateStudent(studentId, "Alice", "Computer Science", 2020, updatedCgpa, 2024);
+
+            const student = await universityData.getStudent(studentId);
+            assert.equal(student.cgpa, updatedCgpa, "CGPA should be updated");
+            // Add other assertions as needed
+        });
     });
 
-    it("should fail for invalid student ID", async () => {
-        try {
-            await universityData.addOrUpdateStudent(0, "Bob", "Physics", 2022, 300, 0);
-            assert.fail("The contract did not throw with an invalid student ID");
-        } catch (err) {
-            assert.include(err.message, "Invalid Student ID", "The error message should contain 'Invalid Student ID'");
-        }
-    });
-
-    it("should fail for invalid CGPA", async () => {
-        try {
-            await universityData.addOrUpdateStudent(2, "Carol", "Biology", 2022, 500, 0);
-            assert.fail("The contract did not throw with an invalid CGPA");
-        } catch (err) {
-            assert.include(err.message, "Invalid CGPA", "The error message should contain 'Invalid CGPA'");
-        }
-    });
+    // Add more tests for other functionalities
 });
