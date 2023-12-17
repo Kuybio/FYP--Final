29d9f7d10480cceb0b29436b6a705efbd2745d92
Kuybio <veriko10c@gmail.com>
2023-12-18 05:13:25 +0800

    Push 34
    Update on syncService.js

--- server/syncService.js
+++ server/syncService.js
@@ -5,97 +5,77 @@ const blockchain = require('../server/blockchain'); // Update the path as necess
 const fromAddress = 0xB4cE6bac673F150ba36D53E3dfd94dCf59a3129c; // Replace with your Ethereum account address
 const privateKey = 0x6449d67debeb3b9471474b20b74b5e04ebbcf4c97ff48c8dde24b1257f07ad6b; // Replace with your private key
 
-// Function to fetch new students who are not yet synced with the blockchain
-const fetchNewStudents = () => {
-    return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM Students WHERE syncedWithBlockchain = FALSE';
-        database.query(query, (error, results) => {
-            if (error) {
-                console.error('Error fetching new students:', error);
-                reject(error);
-            } else {
-                resolve(Array.isArray(results) ? results : []);
-            }
-        });
-    });
-};
+// Function to fetch new students from the database
+async function fetchNewStudents() {
+    const query = 'SELECT * FROM Students WHERE syncedWithBlockchain = FALSE';
+    // Add your database query logic here and return the result
+}
 
-// Function to fetch students who need to be updated on the blockchain
-const fetchStudentChanges = () => {
-    return new Promise((resolve, reject) => {
-        const query = 'SELECT * FROM ChangeLog WHERE synced = FALSE';
-        database.query(query, (error, results) => {
-            if (error) {
-                console.error('Error fetching student changes:', error);
-                reject(error);
-            } else {
-                resolve(Array.isArray(results) ? results : []);
-            }
-        });
-    });
-};
+// Function to fetch updated student changes from the ChangeLog table
+async function fetchUpdatedStudents() {
+    const query = 'SELECT * FROM ChangeLog WHERE synced = FALSE';
+    // Add your database query logic here and return the result
+}
+
+// Helper function to get complete student data by ID
+async function getStudentData(studentId) {
+    const query = 'SELECT * FROM Students WHERE studentId = ?';
+    // Add your database query logic here and return the result
+}
 
 // Function to sync new students with the blockchain
-const syncNewStudentsWithBlockchain = async () => {
+async function syncNewStudentsWithBlockchain() {
     const newStudents = await fetchNewStudents();
     for (const student of newStudents) {
         try {
-            await blockchain.addStudent(student.studentId, student.name, student.programme, student.joinYear, student.cgpa || 0, student.graduateYear || 0);
-            // Mark these students as synced in the database...
+            // Make sure all data is defined
+            const studentData = {
+                studentId: student.studentId,
+                name: student.name,
+                programme: student.programme,
+                joinYear: student.joinYear,
+                cgpa: student.cgpa || 0, // Default to 0 if undefined
+                graduateYear: student.graduateYear || 0 // Default to 0 if undefined
+            };
+
+            // Use your blockchain interaction module to add the student
+            const tx = await blockchain.addStudent(studentData);
+            console.log(`Student ${student.studentId} added to the blockchain with transaction: ${tx}`);
+            // Update the database to mark the student as synced
         } catch (error) {
-            console.error(`Failed to sync new student ${student.studentId}:`, error);
+            console.error(`Failed to sync new student ${student.studentId}: ${error}`);
         }
     }
-};
+}
 
 // Function to sync updated students with the blockchain
-const syncUpdatedStudentsWithBlockchain = async () => {
-    try {
-        const studentChanges = await fetchStudentChanges();
-        for (const change of studentChanges) {
-            try {
-                // Assuming that the student data in the main table is already updated
-                // Fetch the updated student data from the main Students table
-                const student = await getStudentData(change.studentId);
-                const result = await blockchain.updateStudent(
-                    fromAddress,
-                    privateKey,
-                    student.studentId,
-                    student.name,
-                    student.programme,
-                    student.joinYear,
-                    student.cgpa || 0,
-                    student.graduateYear || 0
-                );
-                console.log(`Updated student ${change.studentId} synced successfully with tx: ${result.transactionHash}`);
-                // Update the ChangeLog table to mark the change as synced
-                await markChangeAsSynced(change.id);
-            } catch (error) {
-                console.error(`Failed to sync change for student ${change.studentId}:`, error);
-            }
+async function syncUpdatedStudentsWithBlockchain() {
+    const updatedStudents = await fetchUpdatedStudents();
+    for (const change of updatedStudents) {
+        try {
+            const student = await getStudentData(change.studentId);
+            const tx = await blockchain.updateStudent({
+                studentId: student.studentId,
+                name: student.name,
+                programme: student.programme,
+                joinYear: student.joinYear,
+                cgpa: student.cgpa || 0, // Default to 0 if undefined
+                graduateYear: student.graduateYear || 0 // Default to 0 if undefined
+            });
+            console.log(`Student ${change.studentId} updated on the blockchain with transaction: ${tx}`);
+            // Update the ChangeLog to mark the change as synced
+        } catch (error) {
+            console.error(`Failed to sync updated student ${change.studentId}: ${error}`);
         }
-    } catch (error) {
-        console.error('Error in syncing updated students:', error);
     }
-};
-
-// Function to mark a change as synced in the ChangeLog
-const markChangeAsSynced = (changeId) => {
-    return new Promise((resolve, reject) => {
-        const query = 'UPDATE ChangeLog SET synced = TRUE WHERE id = ?';
-        database.query(query, [changeId], (error, results) => {
-            if (error) {
-                console.error('Error marking change as synced:', error);
-                reject(error);
-            } else {
-                resolve();
-            }
-        });
-    });
-};
+}
 
 // Schedule the sync jobs
 setInterval(syncNewStudentsWithBlockchain, 30000); // Sync new students every 30 seconds
 setInterval(syncUpdatedStudentsWithBlockchain, 30000); // Sync updated students every 30 seconds
 
-module.exports = { syncNewStudentsWithBlockchain, syncUpdatedStudentsWithBlockchain };
+// Export the functions for use in server.js
+module.exports = {
+    syncNewStudentsWithBlockchain,
+    syncUpdatedStudentsWithBlockchain
+};
