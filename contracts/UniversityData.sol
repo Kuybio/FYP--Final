0b94e76127f7d59aa1a776cd404b8a11deb1a305
Kuybio <veriko10c@gmail.com>
2023-12-18 19:57:35 +0800

    Update UniversityData.sol

--- contracts/UniversityData.sol
+++ contracts/UniversityData.sol
@@ -18,23 +18,23 @@ contract UniversityData {
 
     // Add a new student
     function addStudent(uint256 _studentId, string memory _name, string memory _programme, uint256 _joinYear, uint256 _cgpa, uint256 _graduateYear) public {
-         require(_studentId != 0, "Invalid Student ID");
+        require(_studentId != 0, "Invalid Student ID");
         require(_cgpa <= 400, "Invalid CGPA");
         require(students[_studentId].studentId == 0, "Student already exists");
-        
+
         bytes32 dataHash = sha256(abi.encodePacked(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear));
         students[_studentId] = Student(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear, dataHash);
-        
+
         emit StudentAdded(_studentId, dataHash);
     }
 
     // Update an existing student
     function updateStudent(uint256 _studentId, string memory _name, string memory _programme, uint256 _joinYear, uint256 _cgpa, uint256 _graduateYear) public {
         require(students[_studentId].studentId != 0, "Student does not exist");
-        
+
         bytes32 dataHash = sha256(abi.encodePacked(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear));
         students[_studentId] = Student(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear, dataHash);
-        
+
         emit StudentUpdated(_studentId, dataHash);
     }
 
