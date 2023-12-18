af9ac5e036f58cab185d7348a9028ef3ea51d7f7
Kuybio <veriko10c@gmail.com>
2023-12-19 00:13:02 +0800

    blockchain.js

--- server/blockchain.js
+++ server/blockchain.js
@@ -8,9 +8,6 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
 // Create a contract instance
 const universityDataContract = new web3.eth.Contract(universityDataABI, contractAddress);
 
-const fromAddress = '0xB4cE6bac673F150ba36D53E3dfd94dCf59a3129c'; 
-const privateKey = '0x6449d67debeb3b9471474b20b74b5e04ebbcf4c97ff48c8dde24b1257f07ad6b'; 
-
 // Function to add a new student
 async function addStudent(studentId, name, programme, joinYear, cgpa, graduateYear, fromAddress, privateKey) {
     try {
