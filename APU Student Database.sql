8cbbac746573e9d86c22b8de94cb3b0daa0a4c72
Kuybio <veriko10c@gmail.com>
2023-12-18 18:28:59 +0800

    Push 69
    update on database.sql

--- APU Student Database.sql
+++ APU Student Database.sql
@@ -11,6 +11,11 @@ CREATE TABLE Students (
     graduateYear INT NULL  -- Graduate year is nullable and will be updated later
 );
 
+ALTER TABLE Students ADD COLUMN isNew BOOLEAN DEFAULT TRUE;
+
+SELECT * FROM STUDENTS;
+SELECT * FROM ChangeLog;
+
 INSERT INTO Students (studentId, name, programme, joinYear, cgpa, graduateYear) VALUES
 (1, 'John Doe', 'Computer Science', 2020, 350, 2024),
 (2, 'Jane Smith', 'Mechanical Engineering', 2021, NULL, NULL),
@@ -22,3 +27,17 @@ INSERT INTO Students (studentId, name, programme, joinYear, cgpa, graduateYear) 
 (8, 'Linda Taylor', 'English Literature', 2022, NULL, NULL),
 (9, 'David Miller', 'Political Science', 2020, 365, NULL),
 (10, 'Emma Anderson', 'Art History', 2021, NULL, NULL);
+
+
+INSERT INTO Students (studentId, name, programme, joinYear, cgpa, graduateYear) VALUES
+(29, 'Thall', 'Game Developer', 2019, NULL, NULL);
+
+UPDATE Students 
+SET cgpa = 0, graduateYear = 0 
+WHERE studentId = 27;
+
+ALTER TABLE Students DROP COLUMN isNew;
+ALTER TABLE Students DROP COLUMN isUpdated;
+
+ALTER TABLE Students ADD COLUMN isNew BOOLEAN DEFAULT 0;
+ALTER TABLE Students ADD COLUMN isUpdated BOOLEAN DEFAULT 0;
