68be10efb9f5f7b9d7752cb691ae69bd8df8ff3c
Kuybio <veriko10c@gmail.com>
2023-12-18 19:22:28 +0800

    Push 77
    Update on universitydata.sol

--- contracts/UniversityData.sol
+++ contracts/UniversityData.sol
@@ -17,11 +17,14 @@ contract UniversityData {
     event StudentUpdated(uint256 indexed studentId, bytes32 dataHash);
 
     // Add a new student
-    function addStudent(uint256 _studentId, string memory _name, string memory _programme, uint256 _joinYear, uint256 _cgpa, uint256 _graduateYear) public {
+        function addStudent(uint256 _studentId, string memory _name, string memory _programme, uint256 _joinYear, uint256 _cgpa, uint256 _graduateYear) public {
+        require(_studentId != 0, "Invalid Student ID");
+        require(_cgpa <= 400, "Invalid CGPA");
         require(students[_studentId].studentId == 0, "Student already exists");
-        
+
+        students[_studentId] = Student(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear);
         bytes32 dataHash = sha256(abi.encodePacked(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear));
-        students[_studentId] = Student(_studentId, _name, _programme, _joinYear, _cgpa, _graduateYear, dataHash);
+        studentDataHashes[_studentId] = dataHash;
         
         emit StudentAdded(_studentId, dataHash);
     }
