f8f76c60ae9f3341e3d66c3ef85c5572768cd2e1
Kuybio <veriko10c@gmail.com>
2023-12-18 21:35:46 +0800

    Update blockchain.js

--- server/blockchain.js
+++ server/blockchain.js
@@ -8,12 +8,11 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
 // Create a contract instance
 const UniversityDataContract = new web3.eth.Contract(universityDataABI, contractAddress);
 
-// Function to add a new student with specified gas limit
+// Function to add a new student with a fixed gas limit
 async function addStudent(studentId, name, programme, joinYear, cgpa, graduateYear) {
     try {
         const accounts = await web3.eth.getAccounts();
-        const estimatedGas = await UniversityDataContract.methods.addStudent(studentId, name, programme, joinYear, cgpa, graduateYear).estimateGas({ from: accounts[0] });
-        const result = await UniversityDataContract.methods.addStudent(studentId, name, programme, joinYear, cgpa, graduateYear).send({ from: accounts[0], gas: estimatedGas });
+        const result = await UniversityDataContract.methods.addStudent(studentId, name, programme, joinYear, cgpa, graduateYear).send({ from: accounts[0], gas: 2000000 });
         console.log(`Student added: ${result.transactionHash}`);
         return result;
     } catch (error) {
@@ -22,12 +21,11 @@ async function addStudent(studentId, name, programme, joinYear, cgpa, graduateYe
     }
 }
 
-// Function to update an existing student with specified gas limit
+// Function to update an existing student with a fixed gas limit
 async function updateStudent(studentId, name, programme, joinYear, cgpa, graduateYear) {
     try {
         const accounts = await web3.eth.getAccounts();
-        const estimatedGas = await UniversityDataContract.methods.updateStudent(studentId, name, programme, joinYear, cgpa, graduateYear).estimateGas({ from: accounts[0] });
-        const result = await UniversityDataContract.methods.updateStudent(studentId, name, programme, joinYear, cgpa, graduateYear).send({ from: accounts[0], gas: estimatedGas });
+        const result = await UniversityDataContract.methods.updateStudent(studentId, name, programme, joinYear, cgpa, graduateYear).send({ from: accounts[0], gas: 2000000 });
         console.log(`Student updated: ${result.transactionHash}`);
         return result;
     } catch (error) {
