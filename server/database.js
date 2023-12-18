e65e130360af8ab5ea127e3b12cdc04aa0a0e869
Kuybio <veriko10c@gmail.com>
2023-12-18 18:20:16 +0800

    Push 65
    Update on database.js

--- server/database.js
+++ server/database.js
@@ -1,32 +1,71 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const dbConfig = {
   host     : 'localhost', // or your database host
   user     : 'root',
   password : '1Metagross?',
   database : 'UNI'
-});
+};
+
+// Create a MySQL connection
+const connection = mysql.createConnection(dbConfig);
 
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to MySQL Database!');
 });
 
-// Function to execute a query with placeholders and return a promise
-function execute(query, values) {
+// Fetch new students who haven't been synced with the blockchain yet
+function getNewStudents() {
   return new Promise((resolve, reject) => {
-      connection.query(query, values, (error, results, fields) => {
-          if (error) {
-              reject(error);
-              return;
-          }
-          resolve(results);
-      });
+    const query = 'SELECT * FROM Students WHERE isNew = 1';
+    connection.query(query, (err, results) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results);
+    });
   });
 }
 
-// Export the execute function
+// Fetch updated students who haven't been synced with the blockchain yet
+function getUpdatedStudents() {
+  return new Promise((resolve, reject) => {
+    const query = 'SELECT * FROM Students WHERE isUpdated = 1';
+    connection.query(query, (err, results) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results);
+    });
+  });
+}
+
+// Mark a student as synced in the database
+function markStudentSynced(studentId, type) {
+  return new Promise((resolve, reject) => {
+    let query;
+    if (type === 'new') {
+      query = 'UPDATE Students SET isNew = 0 WHERE studentId = ?';
+    } else if (type === 'update') {
+      query = 'UPDATE Students SET isUpdated = 0 WHERE studentId = ?';
+    }
+
+    connection.query(query, [studentId], (err, results) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results);
+    });
+  });
+}
+
 module.exports = {
-  execute,
+  getNewStudents,
+  getUpdatedStudents,
+  markStudentSynced,
   connection
 };
